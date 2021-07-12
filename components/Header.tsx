eeca2138ea9d244b91308127f7c4f93704796c2e
Erik Nilsson <eriks@mail.se>
2021-07-12 03:17:35 +0200

    updated eslint

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,20 +7,18 @@ const HeaderContainer = styled.div`
 `;
 
 const Title = styled.h1`
-  color: black;
   font-size: 48px;
 `;
 
 const Subtitle = styled.h2`
-  color: rgb(10, 70, 228);
-  font-size: 48px;
+  font-size: 24px;
 `;
 
 const Header = () => {
   return (
     <HeaderContainer>
       <Title>High fortune</Title>
-      <Subtitle>w/ Dree Low</Subtitle>
+      <Subtitle>with Dree Low</Subtitle>
     </HeaderContainer>
   );
 };
