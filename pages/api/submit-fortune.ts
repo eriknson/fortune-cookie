b5b8b424a4cf0da9741d065f233b2ffce479de71
eriknson <eriks@mail.se>
2021-11-28 18:36:09 +0100

    lint

--- pages/api/submit-fortune.ts
+++ pages/api/submit-fortune.ts
@@ -1,10 +1,15 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
 const { Client } = require('@notionhq/client');
 
 const notion = new Client({
   auth: process.env.NOTION_API_KEY
 });
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
   if (req.method !== 'POST') {
     return res
       .status(405)
@@ -46,8 +51,8 @@ export default async function handler(req, res) {
         }
       }
     });
-    res.status(201).json({ msg: 'Success' });
+    return res.status(201).json({ msg: 'Success' });
   } catch (error) {
-    res.status(500).json({ msg: 'There was an error' });
+    return res.status(500).json({ msg: 'There was an error' });
   }
 }
